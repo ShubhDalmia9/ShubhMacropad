479d769feb3e29bda316456e1447cb339df0b213
ShubhDalmia9 <shubhdalmia8@gmail.com>
2026-03-29 12:28:55 +0530

    Add files via upload

---
+++ firmware/shubhmacropad/config.h
@@ -0,0 +1,10 @@
+#define ENCODER_A_PINS { GP10 }
+#define ENCODER_B_PINS { GP9 }
+
+#define ENCODER_RESOLUTION 4
+#define VENDOR_ID 0xFEED
+#define PRODUCT_ID 0x0001
+#define DEVICE_VER 0x0001
+
+#define MANUFACTURER "Shubh"
+#define PRODUCT "ShubhMacropad"
